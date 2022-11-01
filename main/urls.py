517185f58b42997fc468deee9193c6fe0ee51170
Abid Ali KP <76676547+abidalikp@users.noreply.github.com>
2022-11-01 17:33:24 +0530

    Finale

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from main import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('projects', views.projects, name='projects'),
+    path('languages', views.languages, name='languages'),
+    path('mytest', views.mytest, name='mytest'),
+]
